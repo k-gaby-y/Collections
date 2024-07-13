2cb557576341cdba06003e216dfad5544ced3961
Gabriely <monjack23@gmail.com>
2024-07-13 17:38:51 -0300

    estudo collection Set

---
+++ src/main.java/set/Ordenacao/CadastroProdutos.java
@@ -0,0 +1,41 @@
+import java.util.HashSet;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class CadastroProdutos {
+    //atributo
+    private Set<Produto> produtoSet;
+
+    public CadastroProdutos(){
+        this.produtoSet = new HashSet<>();
+    }
+
+
+    public void adicionarProduto(long codigo, String nome, double preco, int quantidade){
+        produtoSet.add(new Produto(codigo, nome, preco, quantidade));
+    }
+
+    public Set<Produto> exibirProdutosPorNome(){
+        Set<Produto> produtosPorNome = new TreeSet<>(produtoSet);
+        return produtosPorNome;
+    }
+    public Set<Produto> exibirProdutosPorPreco (){
+        Set<Produto> produtosPorPreco = new TreeSet<>(new ComparatorPorPreco());
+        produtosPorPreco.addAll(produtoSet);
+        return produtosPorPreco;
+    }
+
+    public static void main(String[] args) {
+        CadastroProdutos cadastroProdutos = new CadastroProdutos();
+
+        cadastroProdutos.adicionarProduto(1L, "Produto 1", 15d, 2);
+        cadastroProdutos.adicionarProduto(5L, "Produto 2", 27d, 4);
+        
+        //System.out.println(cadastroProdutos.produtoSet);
+
+        //System.out.println(cadastroProdutos.exibirProdutosPorNome());
+
+        System.out.println(cadastroProdutos.exibirProdutosPorPreco());
+    
+    }
+}
